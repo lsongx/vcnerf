4dacf3c6784de58fdf5b99dc6888bdecf0c3e938
LcDog <12553253+LcDog@users.noreply.github.com>
2021-09-23 21:08:04 -0400

    render-pose

--- vcnerf/datasets/utils/llff_loader.py
+++ vcnerf/datasets/utils/llff_loader.py
@@ -158,9 +158,9 @@ def render_path_spiral(c2w, up, rads, focal, zdelta, zrate, rots, N):
     
     for theta in np.linspace(0., 2. * np.pi * rots, N+1)[:-1]:
         c = np.dot(c2w[:3,:4], np.array([np.cos(theta), -np.sin(theta), -np.sin(theta*zrate), 1.]) * rads)
-        c[0] -= 0.5
-        # focal *= 0.5
-        up=[1,0,0]
+        # c[0] -= 0.5
+        # # focal *= 0.5
+        # up=[1,0,0]
         z = normalize(c - np.dot(c2w[:3,:4], np.array([0,0,-focal, 1.])))
         render_poses.append(np.concatenate([viewmatrix(z, up, c), hwf], 1))
     return render_poses
